d324ee122b843b3230a8cf731ca682f44840a1ae
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-25 02:01:26 +0000

    Add printf function files

---
+++ holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLB_H
+#define HOLB_H
+#include <stdarg.h>
+/**
+ *struct spec - printf specifiers
+ *@s: character specifier
+ *@print: function pointer to a format function
+ *
+ *Description: structure holds a character specifier and a format function
+ */
+typedef struct spec {
+	char *s;
+	int (*printspec)(va_list);
+} specifiers;
+
+int _printf(const char *format, ...);
+int printchar(va_list list);
+int printstr(va_list list);
+int _putchar(char c);
+#endif
